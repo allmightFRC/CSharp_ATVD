253b0ecd107b31c0696672fa5721203141675e89
Niord <niordpereira@gmail.com>
2024-08-12 08:46:40 -0300

    unico campeão do estado

---
+++ ATVD1/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("É campeão");
+        Console.WriteLine("BICAMPEÃO SÓ A PARANGABA");
+        Console.ReadLine();
+    }
+}
